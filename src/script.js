5005881322073fde417ed4c247ed0ff55b05f97a
thaisc98 <thaisc98@hotmail.es>
2025-01-03 15:27:14 -0400

    [NEW] Resizing

--- src/script.js
+++ src/script.js
@@ -1,44 +1,59 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-console.log(OrbitControls)
+
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "#74b3ce"})
+const cubeGeometry = new THREE.BoxGeometry(1,1,1);
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "#74b3ce"});
 
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
-)
-scene.add(cubeMesh)
+);
+scene.add(cubeMesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
   75, 
   window.innerWidth / window.innerHeight,
   0.1,
-  200)
-camera.position.z = 5
+  200);
+
+// const aspectRatio = window.innerWidth / window.innerHeight
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   200
+// )
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector('canvas.threejs')
+const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
+});
+
 
 //instantiate the controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-controls.autoRotate = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
 
 
 const renderloop = () => {
-  console.log("renderloop")
-  controls.update()
-  renderer.render(scene, camera)
-  window.requestAnimationFrame(renderloop)
-}
-renderloop()
+  console.log("renderloop");
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
