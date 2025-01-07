08b8faa84c5ca4624dcef9349bdf4da50dc7c4ca
thaisc98 <thaisc98@hotmail.es>
2025-01-07 00:01:44 -0400

    [NEW] wireframe

--- src/script.js
+++ src/script.js
@@ -6,36 +6,14 @@ const scene = new THREE.Scene();
 
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "#74b3ce"});
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "#74b3ce", wireframe: true});
 
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 );
-
-cubeMesh.position.y = -1;
-cubeMesh.scale.setScalar(0.5);
-
-const cubeMesh2 = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-);
-cubeMesh2.position.x = 2;
-const cubeMesh3 = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-);
-cubeMesh3.position.x = -2;
-
-const group = new THREE.Group();
-group.add(cubeMesh);
-group.add(cubeMesh2);
-group.add(cubeMesh3);
-
-group.position.y = 2;
-group.scale.setScalar(2);
-scene.add(group);
-//scene.add(cubeMesh);
+cubeMesh.position.y =1;
+scene.add(cubeMesh);
 
 //cubeMesh.scale.set(2,2,1);
 // const tempVector = new THREE.Vector3(0,1,0);
