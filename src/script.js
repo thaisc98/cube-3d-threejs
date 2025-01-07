630f977bb4fe8948428bb3b62ea6d168cf98affe
thaisc98 <thaisc98@hotmail.es>
2025-01-06 20:55:51 -0400

    [FIX] antialiasing

--- src/script.js
+++ src/script.js
@@ -35,22 +35,27 @@ camera.position.z = 5;
 // initialize the renderer
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias:true,
 });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 
 //instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.autoRotate = true;
+//controls.autoRotate = true;
 
-
-const renderloop = () => {
-  console.log("renderloop");
+window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+// render the scene
+const renderloop = () => {
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
