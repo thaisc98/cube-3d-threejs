b2eb47baccce002c53b2eda356caf90adb3682ce
thaisc98 <thaisc98@hotmail.es>
2025-01-06 23:56:53 -0400

    [NEW] scene hierarchy

--- src/script.js
+++ src/script.js
@@ -12,7 +12,43 @@ const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 );
-scene.add(cubeMesh);
+
+cubeMesh.position.y = -1;
+
+const cubeMesh2 = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+);
+cubeMesh2.position.x = 2;
+const cubeMesh3 = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+);
+cubeMesh3.position.x = -2;
+
+const group = new THREE.Group();
+group.add(cubeMesh);
+group.add(cubeMesh2);
+group.add(cubeMesh3);
+
+group.position.y = 2;
+
+scene.add(group);
+//scene.add(cubeMesh);
+
+//cubeMesh.scale.set(2,2,1);
+// const tempVector = new THREE.Vector3(0,1,0);
+// cubeMesh.position.copy(tempVector);
+
+//Red = X, Green = Y, Blue = Z
+// cubeMesh.position.y = 2;
+// cubeMesh.position.x = 2;
+// cubeMesh.position.z = 2;
+
+console.log(cubeMesh);
+
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -21,22 +57,13 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   200);
 
-// const aspectRatio = window.innerWidth / window.innerHeight
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   200
-// )
 camera.position.z = 5;
 
 // initialize the renderer
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  antialias:true,
+  antialias: true,
 });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
