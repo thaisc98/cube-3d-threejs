3e5101673d6db7ace3eb46843ad5474d053d6f66
thaisc98 <thaisc98@hotmail.es>
2025-01-07 23:52:55 -0400

    [rmv] clock

--- src/script.js
+++ src/script.js
@@ -7,31 +7,12 @@ const scene = new THREE.Scene();
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "#74b3ce", wireframe: true});
-
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 );
 
-// cubeMesh.rotation.reorder('YXZ');
-// cubeMesh.rotation.y = THREE.MathUtils.degToRad(90);
-// cubeMesh.rotation.x = THREE.MathUtils.degToRad(45);
-
 scene.add(cubeMesh);
-
-//cubeMesh.scale.set(2,2,1);
-// const tempVector = new THREE.Vector3(0,1,0);
-// cubeMesh.position.copy(tempVector);
-
-//Red = X, Green = Y, Blue = Z
-// cubeMesh.position.y = 2;
-// cubeMesh.position.x = 2;
-// cubeMesh.position.z = 2;
-
-console.log(cubeMesh);
-
-//const axesHelper = new THREE.AxesHelper(2);
-//scene.add(axesHelper);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -55,7 +36,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 //instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-//controls.autoRotate = true;
+controls.autoRotate = true;
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -63,19 +44,9 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
-//initiatilize the clock
-const clock = new THREE.Clock();
-let previousTime = 0;
 
 // render the scene
 const renderloop = () => {
-  const currentTime = clock.getElapsedTime();
-  const delta = currentTime - previousTime;
-  previousTime = currentTime;
-
-  cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 20;
-  cubeMesh.scale.x = Math.sin(currentTime);
-  
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
