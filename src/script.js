7e71c8c5e5a68e0f7cc3ec741cda286cd1e4b164
thaisc98 <thaisc98@hotmail.es>
2025-01-20 15:18:34 -0400

    [NEW] fog and side

--- src/script.js
+++ src/script.js
@@ -6,28 +6,29 @@ const scene = new THREE.Scene();
 
 // add objects to the scene
 const geometry = new THREE.BoxGeometry(1,1,1,2,2,2);
-//const geometry = new THREE.SphereGeometry(1,16,18);
-//const geometry = new THREE.PlaneGeometry(1,1,2,2);
-//const geometry = new THREE.TorusKnotGeometry(10,3,100,16);
+const planeGeometry = new THREE.PlaneGeometry(1,1);
 
+const material = new THREE.MeshBasicMaterial({color: "#74b3ce", wireframe: false});
 
-//create custom geometry
-// const vertices = new Float32Array([
-//   0,0,0,
-//   0,2,0,
-//   2,0,0
-// ]);
-// const bufferAttribute = new THREE.BufferAttribute(vertices, 3);
-// const geometry = new THREE.BufferGeometry();
-// geometry.setAttribute('position', bufferAttribute);
+material.transparent = true;
+material.opacity = 0.5;
+material.side = THREE.DoubleSide;
+material.fog = true;
 
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "#74b3ce", wireframe: true});
-const cubeMesh = new THREE.Mesh(
-  geometry,
-  cubeMaterial
-);
+const fog = new THREE.Fog(0xffffff,1,10);
+scene.fog = fog;
+scene.background = new THREE.Color(0xffffff);
+const cubeMesh = new THREE.Mesh(geometry, material);
+
+const mesh2 = new THREE.Mesh(geometry, material);
+mesh2.position.x = 1.5;
+
+const planeMesh = new THREE.Mesh(planeGeometry,material);
+planeMesh.position.x = -1.5;
 
 scene.add(cubeMesh);
+scene.add(mesh2);
+scene.add(planeMesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -51,7 +52,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 //instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-//controls.autoRotate = true;
+controls.autoRotate = true;
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
